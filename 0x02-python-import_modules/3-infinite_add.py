652195762f927fd012acb9a771b558cef1068c91
ubaka365 <stephen.ubaka@gmail.com>
2023-08-11 17:47:12 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3<F11>
 import sys
-if __name__ != "__main__":<F11>
+if __name__ != "__main__":
     exit()
-    argc = len(sys.argv) - 1<F11>
+    argc = len(sys.argv) - 1
+
     i = 0
     result = 0
     for arg in sys.argv:
