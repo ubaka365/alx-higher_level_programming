2d00e720b335eb3cf1b7789f586b16d5534ba43d
ubaka365 <stephen.ubaka@gmail.com>
2023-10-19 14:36:24 -0700

    Final half of task

--- 0x02-python_import_modules/README.md
+++
@@ -1,1 +0,0 @@
-0x02-python_import_modules
---
+++ 0x04-python-more_data_structures/python.h
@@ -0,0 +1,9 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+#include <stdlib.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
+
+#endif
